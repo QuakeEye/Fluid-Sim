befb01634f122aa25ce72606ab15a5359924aadd
Quake <guidomintjes@gmail.com>
2023-03-04 18:49:47 +0100

    Added world to target rendering, including scaling

--- Sim.cs
+++ Sim.cs
@@ -16,7 +16,11 @@ namespace Fluid_Sim {
         public Sim(Surface target, Vector2i spaceSize) {
 
             renderTarget = target;
+
+            // Save the world size and set up the integer array
             simSpaceSize = spaceSize;
+            simSpace = new int[simSpaceSize.X, simSpaceSize.Y];
+            simSpace = Utils.Populate2DArray(simSpace, int.MaxValue);
         }
 
 
@@ -24,7 +28,7 @@ namespace Fluid_Sim {
         //  should be called every frame
         public void Update() {
 
-            Utils.SetRandomColour(renderTarget);
+            Utils.SetRandomColour(simSpace);
         }
 
 
@@ -32,7 +36,32 @@ namespace Fluid_Sim {
         //  in a scalable manner
         public void Render() {
 
-            // todo
+            // Big warning: widths and heights of render target should be some multiplication
+            //  of the world size, as information might be lost if this is not the case
+
+            // Slight to-do: this scaling code should propably occur once, and on window resize
+
+            // This scaling should be a multiple idea is enhanced by the fact that the scales
+            //  are stored as integers, so watch out!
+            int widthScaler = renderTarget.Width / simSpaceSize.X;
+            int heightScaler = renderTarget.Height / simSpaceSize.Y;
+
+            // Render out the world into the target in *some* way
+            for (int x = 0; x < simSpaceSize.X * widthScaler; x += widthScaler) {
+                for (int y = 0; y < simSpaceSize.Y * heightScaler; y += heightScaler) {
+
+                    // Fill out this kind of box in the render target that is the world scale size
+                    for (int i = 0; i < widthScaler; i++) {
+                        for (int j = 0; j < heightScaler; j++) {
+
+                            // For now, just plot this arbitrary pixel colour directly
+                            renderTarget.Plot(  x + i, 
+                                                y + j, 
+                                                simSpace[x / widthScaler, y / heightScaler]);
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- Utils.cs
+++ Utils.cs
@@ -28,5 +28,38 @@ namespace Fluid_Sim {
 
             surf.Plot(x, y, mixColour(randomColour));
         }
+
+
+        /// <summary>
+        /// Overload for the function which does the exact same as above,
+        ///  but for a 2dimensional array
+        /// </summary>
+        public static void SetRandomColour(int[,] arr) {
+
+            // Assign a random pixel in array with a random colour
+            int x = Globals.random.Next(0, arr.GetLength(0));
+            int y = Globals.random.Next(0, arr.GetLength(1));
+
+            Vector3 randomColour = new Vector3(
+                (float) Globals.random.NextDouble() * 255f,
+                (float) Globals.random.NextDouble() * 255f,
+                (float) Globals.random.NextDouble() * 255f
+            );
+
+            arr[x, y] = mixColour(randomColour);
+        }
+
+
+        /// <summary>
+        /// Helper function to populate a 2d array with some value
+        /// <summary>
+        public static T[,] Populate2DArray<T>(T[,] array, T value) {
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    array[i, j] = value;
+
+            return array;
+        }
     }
 }
